e9fd5cdf665a11e531441620fa3c29a839595388
Jonathan George <jon@jonathangeorge.co.uk>
2020-12-11 13:56:54 +0000

    Make 11.1 less bad.

--- AoC2020.Solutions/Day11/Part01.cs
+++ AoC2020.Solutions/Day11/Part01.cs
@@ -1,7 +1,9 @@
-﻿namespace AoC2020.Solutions.Day11
+﻿#pragma warning disable SA1011 // Closing square brackets should be spaced correctly
+namespace AoC2020.Solutions.Day11
 {
     using System;
     using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
     using System.Linq;
 
     public class Part01 : ISolution
@@ -12,131 +14,108 @@
 
         public string Solve(string input)
         {
-            this.totalColumns = input.IndexOf(Environment.NewLine);
-
             // Get rid of the newlines
-            char[] grid = input.Replace(Environment.NewLine, string.Empty).ToCharArray();
+            char[][] grid = input
+                .Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => x.ToCharArray())
+                .ToArray();
 
-            this.totalRows = grid.Length / this.totalColumns;
-
-            // Check....
-            if (grid.Length % this.totalColumns != 0)
-            {
-                throw new Exception("Something's wrong with either the code or the input. It's probably the code.");
-            }
-
-            string last = string.Concat(grid);
+            this.totalRows = grid.Length;
+            this.totalColumns = grid[0].Length;
 
             while (true)
             {
-                char[] next = this.NextGeneration(grid);
-                string nextString = string.Concat(next);
-
-                if (nextString == last)
+                if (!this.TryGetNextGeneration(grid, out char[][]? next))
                 {
-                    return next.Count(x => x == '#').ToString();
+                    return grid.Sum(x => x.Count(y => y == '#')).ToString();
                 }
 
-                last = nextString;
                 grid = next;
             }
         }
 
-        private char[] NextGeneration(char[] current)
+        private bool TryGetNextGeneration(char[][] current, [NotNullWhen(true)] out char[][]? next)
         {
-            char[] result = new char[current.Length];
+            next = new char[this.totalRows][];
+            bool changed = false;
 
-            for (int index = 0; index < current.Length; index++)
+            for (int row = 0; row < this.totalRows; row++)
             {
-                List<int> surroundingSeats = this.GetSurroundingSeatReferences(index);
+                next[row] = new char[this.totalColumns];
 
-                if (current[index] == 'L' && !surroundingSeats.Any(i => current[i] == '#'))
+                for (int column = 0; column < this.totalColumns; column++)
                 {
-                    result[index] = '#';
+                    int surroundingSeatOccupancy = this.GetSurroundingSeatOccupancy(current, row, column);
+                    if (current[row][column] == 'L' && surroundingSeatOccupancy == 0)
+                    {
+                        changed = true;
+                        next[row][column] = '#';
+                    }
+                    else if (current[row][column] == '#' && surroundingSeatOccupancy > 3)
+                    {
+                        changed = true;
+                        next[row][column] = 'L';
+                    }
+                    else
+                    {
+                        next[row][column] = current[row][column];
+                    }
                 }
-                else if (current[index] == '#' && surroundingSeats.Count(i => current[i] == '#') >= 4)
-                {
-                    result[index] = 'L';
-                }
-                else
-                {
-                    result[index] = current[index];
-                }
+            }
+
+            return changed;
+        }
+
+        private int GetSurroundingSeatOccupancy(char[][] data, int row, int column)
+        {
+            int result = 0;
+
+            bool firstCol = column == 0;
+            bool lastCol = column == this.totalColumns - 1;
+            bool firstRow = row == 0;
+            bool lastRow = row == this.totalRows - 1;
+
+            if (!firstRow && !firstCol && data[row - 1][column - 1] == '#')
+            {
+                result++;
+            }
+
+            if (!firstRow && data[row - 1][column] == '#')
+            {
+                result++;
+            }
+
+            if (!firstRow && !lastCol && data[row - 1][column + 1] == '#')
+            {
+                result++;
+            }
+
+            if (!firstCol && data[row][column - 1] == '#')
+            {
+                result++;
+            }
+
+            if (!lastCol && data[row][column + 1] == '#')
+            {
+                result++;
+            }
+
+            if (!lastRow && !firstCol && data[row + 1][column - 1] == '#')
+            {
+                result++;
+            }
+
+            if (!lastRow && data[row + 1][column] == '#')
+            {
+                result++;
+            }
+
+            if (!lastRow && !lastCol && data[row + 1][column + 1] == '#')
+            {
+                result++;
             }
 
             return result;
-        }
-
-        private List<int> GetSurroundingSeatReferences(int index)
-        {
-            return this.GetSurroundingSeatReferences(
-                index % this.totalColumns,
-                index / this.totalColumns);
-        }
-
-        private List<int> GetSurroundingSeatReferences(int currentColumn, int currentRow)
-        {
-            var result = new List<int>();
-
-            bool firstCol = currentColumn == 0;
-            bool lastCol = currentColumn == this.totalColumns - 1;
-            bool firstRow = currentRow == 0;
-            bool lastRow = currentRow == this.totalRows - 1;
-
-            if (!firstRow)
-            {
-                if (!firstCol)
-                {
-                    // Add up-left
-                    result.Add(this.GetSeatIndex(currentColumn - 1, currentRow - 1));
-                }
-
-                // Add up
-                result.Add(this.GetSeatIndex(currentColumn, currentRow - 1));
-
-                if (!lastCol)
-                {
-                    // Add up-right
-                    result.Add(this.GetSeatIndex(currentColumn + 1, currentRow - 1));
-                }
-            }
-
-            if (!firstCol)
-            {
-                // Add left
-                result.Add(this.GetSeatIndex(currentColumn - 1, currentRow));
-            }
-
-            if (!lastCol)
-            {
-                // Add right
-                result.Add(this.GetSeatIndex(currentColumn + 1, currentRow));
-            }
-
-            if (!lastRow)
-            {
-                if (!firstCol)
-                {
-                    // Add down-left
-                    result.Add(this.GetSeatIndex(currentColumn - 1, currentRow + 1));
-                }
-
-                // Add down
-                result.Add(this.GetSeatIndex(currentColumn, currentRow + 1));
-
-                if (!lastCol)
-                {
-                    // Add down-right
-                    result.Add(this.GetSeatIndex(currentColumn + 1, currentRow + 1));
-                }
-            }
-
-            return result;
-        }
-
-        private int GetSeatIndex(int col, int row)
-        {
-            return col + (row * this.totalColumns);
         }
     }
 }
